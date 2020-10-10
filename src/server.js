b380ef6977d3d25482c10e3be190f701130361d8
leilafreitas <leilafreitas159@gmail.com>
2020-10-10 13:34:26 -0300

    'servidoratual'

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-//Puxa o dotenv 
+//Puxa o dotenv para configurar
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -8,10 +8,9 @@ const routes =require('./routes');
 const server = express();
 //Ajuda em termos de cors, podendo facilitar a requisição da api
 server.use(cors());
-//Ajuda com a questão de facilidade de acesso a dados (post,gets...)
+//Ajuda com a questão de facilidade de acesso a dados (post,gets,puts,...)
 server.use(bodyParser.urlencoded({extended:false}));
 server.use('/api',routes);
-
 server.listen(process.env.PORT, ()=>{
-    console.log(`Servidor rodando na porta ${process.env.PORT}`);
+    console.log(`SERVIDOR RODANDO NA PORTA : ${process.env.PORT}`);
 })
